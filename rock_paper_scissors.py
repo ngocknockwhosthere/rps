3a37319a1379e39812981db0888efd146084015a
ngocknockwhosthere <ngocknockwhosthere@gmail.com>
2023-02-17 00:27:07 -0800

    intro

---
+++ rock_paper_scissors.py
@@ -0,0 +1,11 @@
+import random
+
+def main():
+    print("Hello!")
+    print("How to play: Two players secretly pick one of rock, paper, or scissors. Both players reveal their selection to the other player at once; the winner is chosen based on what the selections are. Rock beats scissors (by crushing them); scissors beats paper (by cutting it); and paper beats rock (by covering it). If both players select the same one, it is a tie.")
+    
+    return 0
+
+
+if __name__ == "__main__":
+    main()
