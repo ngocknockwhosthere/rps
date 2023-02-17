8aa8ba6b24e22d318ed2d83c05c2e17fa1c0dad0
ngocknockwhosthere <ngocknockwhosthere@gmail.com>
2023-02-17 00:32:33 -0800

    buggy
    no ties
    
    3rd commit
    
    message
    
    squashing

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,33 +1,35 @@
 import random
 
 def main():
-    print("Hello!")
+    print("Hello")
     print("How to play: Two players secretly pick one of rock, paper, or scissors. Both players reveal their selection to the other player at once; the winner is chosen based on what the selections are. Rock beats scissors (by crushing them); scissors beats paper (by cutting it); and paper beats rock (by covering it). If both players select the same one, it is a tie.")
     user_action = input("Enter a choice (rock, paper, scissors): ")
     possible_actions = ["rock", "paper", "scissors"]
     computer_action = random.choice(possible_actions)
     print("computer chose: " + computer_action)
-    if user_action == "rock": 
-        if computer_action == "paper":
-            print("Computer wins")
-        elif computer_action == " scisscors":
-            print("User wins")
+    status = "Tie"
+    while status != "Tie":
+        if user_action == "rock": 
+            if computer_action == "paper":
+                status = "Computer wins"
+            elif computer_action == " scisscors":
+                status = "User wins"
+            else:
+                status = "Tie"
+        elif user_action == "paper": 
+            if computer_action == "paper":
+                status = "Tie"
+            elif computer_action == " scisscors":
+                status = "Computer wins"
+            else:
+                status = "User wins"
         else:
-            print("Tie")
-    elif user_action == "paper": 
-        if computer_action == "paper":
-            print("Tie")
-        elif computer_action == " scisscors":
-            print("Computer wins")
-        else:
-            print("User wins")
-    else:
-        if computer_action == "paper":
-            print("User wins")
-        elif computer_action == " scisscors":
-            print("Tie")
-        else:
-            print("Computer wins")
+            if computer_action == "paper":
+                status = "User wins"
+            elif computer_action == " scisscors":
+                status = "Tie"
+            else:
+                status = "Computer wins"
     return 0
 
 
